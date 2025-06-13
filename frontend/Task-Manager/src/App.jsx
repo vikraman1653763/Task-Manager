a3a134a82510e3ae06a4515b8e0753014fd1d468
vikraman1653763 <vikraman1653763@gmail.com>
2025-06-13 13:37:51 +0530

    Pages, routes, hooks, creation

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
 
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
 function App() {
   return (
-    <div className='text-4xl'>
-      APPP
+    <div>
+        <Router>
+          <Routes>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/signup' element={<SignUp/>}/>
+            {/* Admin */}
+            <Route element={<PrivateRoute allowedRoles={['admin']}/>}>
+            <Route path='/admin/dashboard' element={<Dashboard/>}/>
+            <Route path='/admin/tasks' element={<ManageTasks/>}/>
+            </Route>
+          </Routes>
+        </Router>
+  
     </div>
   )
 }
